db5ff6a928a38390fa7767a4acc81b96fce65bfa
Beatrice Schuster <tbschuster1@gmail.com>
2022-08-30 23:22:03 +0200

    semi-working script

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if(entry.isIntersecting){
-      document.querySelectorAll(".animated")[0].classList.add("fadeIn")
-      document.querySelectorAll(".animated")[1].classList.add("fadeInRight")
-      document.querySelectorAll(".animated")[2].classList.add("fadeInRight")
-      document.querySelectorAll(".animated")[3].classList.add("fadeInRight")
-    }
-  })
-})
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if(entry.isIntersecting){
+//       document.querySelectorAll(".animated")[0].classList.add("fadeIn")
+//       document.querySelectorAll(".animated")[1].classList.add("fadeInRight")
+//       document.querySelectorAll(".animated")[2].classList.add("fadeInRight")
+//       document.querySelectorAll(".animated")[3].classList.add("fadeInRight")
+//       document.querySelectorAll(".animated")[3].classList.add("fadeInRight")
+//     }
+//   })
+// })
 
-observer.observe(document.querySelector(".experience-container"))
+// observer.observe(document.querySelector(".cd-container"))
